cce5d61670b524c81045aacc8701b91cd4211b84
Ryan Smith <rpsmitty222@gmail.com>
2023-11-30 17:04:59 -0500

    adding user joined

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,9 +1,9 @@
 import message from './message.js'
 import playedSong from './playedSong.js'
-// import userJoined from './userJoined.js'
+import userJoined from './userJoined.js'
 
 export const handlers = {
   message,
-  playedSong
-  // userJoined
+  playedSong,
+  userJoined
 }
