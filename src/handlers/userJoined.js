d229ab77c7350e85ab64e68de00a8a14bbca1994
Ryan Smith <rpsmitty222@gmail.com>
2023-11-30 19:28:35 -0500

    test

--- src/handlers/userJoined.js
+++ src/handlers/userJoined.js
@@ -1,15 +1,33 @@
-import { postMessage } from '../libs/cometchat.js'
+import { postMessage } from '../libs/cometchat.js';
 
 export default async (payload, room) => {
-  if (!payload.userUuid) return
-  if ([process.env.CHAT_USER_ID, process.env.CHAT_REPLY_ID].includes(payload.userUuid)) return
-  postMessage({
-    room,
-    message: `Welcome @${payload.nickname}... feel free to ask me any questions!`,
-    mentions: [{
-      position: 8,
-      nickname: payload.nickname,
-      userId: payload.userUuid
-    }]
-  })
-}
+  try {
+    console.log('User joined handler called:', payload);
+
+    if (!payload.userUuid) {
+      console.log('User UUID not found in payload');
+      return;
+    }
+
+    if ([process.env.CHAT_USER_ID, process.env.CHAT_REPLY_ID].includes(payload.userUuid)) {
+      console.log(`User ${payload.nickname} is excluded from welcome message`);
+      return;
+    }
+
+    console.log('Sending welcome message...');
+    
+    await postMessage({
+      room,
+      message: `Welcome @${payload.nickname}... feel free to ask me any questions!`,
+      mentions: [{
+        position: 8,
+        nickname: payload.nickname,
+        userId: payload.userUuid,
+      }],
+    });
+
+    console.log('Welcome message sent successfully');
+  } catch (error) {
+    console.error('Error in user joined handler:', error.message);
+  }
+};
