8308791b47c523a35a2ee276f4797f130bfc147d
Ryan Smith <rpsmitty222@gmail.com>
2023-11-30 19:15:04 -0500

    test

--- src/handlers/playedSong.js
+++ src/handlers/playedSong.js
@@ -5,12 +5,19 @@ let merchCount = 0;
 let songDetailCount = 0;
 
 export default async (payload, room) => {
+  console.log('songDetailCount:', songDetailCount);
+  console.log('ANNOUNCE_SONG_DETAILS_COUNT:', process.env.ANNOUNCE_SONG_DETAILS_COUNT);
+
   if (++songDetailCount > process.env.ANNOUNCE_SONG_DETAILS_COUNT) {
     songDetailCount = 0;
 
     if (payload?.nowPlaying?.song?.artistName?.toLowerCase().includes(process.env.FAVOURITE_ARTIST.toLowerCase())) {
       const reply = await getTrackFact(`${payload.nowPlaying.song.trackName} by ${payload.nowPlaying.song.artistName}`);
       let messageSuffix = '';
+
+      console.log('merchCount:', merchCount);
+      console.log('MERCH_MESSAGE_RANDOM_SONG_COUNT:', process.env.MERCH_MESSAGE_RANDOM_SONG_COUNT);
+      console.log('Random Message Condition Met:', Math.floor(Math.random() * 2) === 1);
 
       if (++merchCount > process.env.MERCH_MESSAGE_RANDOM_SONG_COUNT) {
         merchCount = 0;
