2d30ae2cd88f9e29d5b2ba5b209e8e19ee8bd484
Ryan Smith <rpsmitty222@gmail.com>
2023-12-01 10:16:17 -0500

    adding console logs to theme.js

--- src/handlers/theme.js
+++ src/handlers/theme.js
@@ -1,12 +1,17 @@
 // handlers/theme.js
-export default async (payload, room) => {
+export default async (payload, room, bot) => {
+  try {
+    console.log('Received theme command:', payload.message);
+
     if (payload.message.startsWith('/settheme')) {
       // Extract the new theme from the command
       const newTheme = payload.message.replace('/settheme', '').trim();
-  
+
+      console.log('Setting new theme:', newTheme);
+
       // Set the new theme
-      this.currentTheme = newTheme;
-  
+      bot.currentTheme = newTheme;
+
       // Respond with a confirmation message
       await postMessage({
         room,
@@ -14,9 +19,14 @@ export default async (payload, room) => {
       });
     } else if (payload.message.startsWith('/gettheme')) {
       // Respond with the current theme
+      console.log('Getting current theme:', bot.currentTheme);
+
       await postMessage({
         room,
-        message: `Current Theme: ${this.currentTheme}`
+        message: `Current Theme: ${bot.currentTheme}`
       });
     }
-  };
+  } catch (error) {
+    console.error('Error in theme handler:', error);
+  }
+};
