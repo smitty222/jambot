7612dbad815b4d3fdeae70a69a3e0d4fd64f29b4
Ryan Smith <rpsmitty222@gmail.com>
2023-12-20 12:38:52 -0500

    test

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,11 +1,12 @@
 import message from './message.js'
 import playedSong from './playedSong.js'
-import userJoined from './userJoined.js'
-
+import { handleUserJoined } from './userJoined.js'
+import { handleUserLeft } from './userLeft.js'
 
 export const handlers = {
   message,
   playedSong,
-  userJoined,
-  
+  handleUserJoined,
+  handleUserLeft,
+
 }
--- src/handlers/userJoined.js
+++ src/handlers/userJoined.js
@@ -1,7 +1,14 @@
 // userJoined.js
 import { postMessage } from '../libs/cometchat.js';
 
-export default async (payload, room) => {
+// Define custom welcome messages for specific users
+const customWelcomeMessages = {
+  '210141ad-6b01-4665-84dc-e47ea7c27dcb': 'Test Test Test Rsmitty has joined', // Rsmitty
+  'user_uuid_2': 'Hello, User2! Enjoy your time in the room and feel free to chat with me.',
+  // Add more user UUIDs and their custom welcome messages as needed
+};
+
+export const handleUserJoined = async (payload, room) => {
   try {
     console.log('User joined handler called:');
     console.log('Received Payload:', payload);
@@ -28,19 +35,25 @@ export default async (payload, room) => {
       return;
     }
 
-    const userDisplayName = userProfile.nickname;
+    // Check if the current joined user is not in the list
+    if (!joinedUserIds.includes(joinedUser.uuid)) {
+      const customWelcomeMessage = customWelcomeMessages[joinedUser.uuid];
 
-    await postMessage({
-      room,
-      message: `Welcome @${userProfile.nickname} . Feel free to ask me any questions by tagging me, or use /commands for a list of commands`,
-      mentions: [{
-        position: 8,
-        nickname: userProfile.nickname,
-        userId: joinedUser.uuid,
-      }],
-    });
+      // Send custom welcome message if available, else send a generic welcome message
+      const welcomeMessage = customWelcomeMessage || `Welcome to the room, ${userProfile.nickname}!`;
 
-    console.log(`Welcome message sent successfully to ${userDisplayName}`);
+      // Send welcome message
+      await postMessage({
+        room,
+        message: welcomeMessage,
+        mentions: customWelcomeMessage ? [] : undefined,
+      });
+
+      console.log(`Welcome message sent successfully to ${userProfile.nickname}`);
+
+      // Update the list of joined users
+      joinedUserIds.push(joinedUser.uuid);
+    }
   } catch (error) {
     console.error('Error sending the welcome message:', error.message);
   }
