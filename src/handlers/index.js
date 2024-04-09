6ea0955311d64c1b7533b2f3452f5b1f056458d4
Ryan Smith <rpsmitty222@gmail.com>
2024-04-09 18:29:51 -0400

    userLeft function to send payload info

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,10 +1,11 @@
 import message from './message.js'
 import userJoined from './userJoined.js'
+import userLeft from './userLeft.js'
 
 
 export const handlers = {
   message,
   userJoined,
- 
+  userLeft,
 
 }
