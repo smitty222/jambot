b2c9fc2aed16bcf6a812da6515b82a39faa629c8
Ryan Smith <rpsmitty222@gmail.com>
2024-04-11 17:50:08 -0400

    s

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import { Chain } from 'repeat'
 import { Bot } from './libs/bot.js'
 
-
 const roomBot = new Bot(process.env.JOIN_ROOM);
 
-// Connect the bot before configuring listeners and repeating tasks
 await roomBot.connect();
 
-// Configure listeners after the bot has connected
 roomBot.configureListeners();
 
-// Add repeated tasks after the bot has connected
 const repeatedTasks = new Chain()
   .add(async () => {
     await roomBot.processNewMessages();
