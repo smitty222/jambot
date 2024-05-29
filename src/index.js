48bccd28dd3b48774a991deed7830bdcbd4b2875
Ryan Smith <rpsmitty222@gmail.com>
2024-05-29 14:56:13 -0400

    updating port listening

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ const repeatedTasks = new Chain()
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
