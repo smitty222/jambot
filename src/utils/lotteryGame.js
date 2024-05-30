72c3e14887b3dc6e00bec67875c7760a6ff9387e
Ryan Smith <rpsmitty222@gmail.com>
2024-05-30 19:34:19 -0400

    lottery fix again

--- src/utils/lotteryGame.js
+++ src/utils/lotteryGame.js
@@ -2,7 +2,7 @@ import { postMessage } from "../libs/cometchat.js";
 import { fetchUserData } from "./API.js";
 
 // Global variables
-const MAX_NUMBER = 1;
+const MAX_NUMBER = 100;
 const MIN_NUMBER = 1;
 const TIMEOUT_DURATION = 30000; // 30 seconds timeout
 const DRAWING_DELAY = 5000; // 5 seconds delay before drawing
