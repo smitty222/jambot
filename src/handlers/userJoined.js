0c52beef7351de40313777bc471d50b04704c0b2
Ryan Smith <rpsmitty222@gmail.com>
2025-07-22 12:24:12 -0400

    Add missing userJoined.js file

--- src/handlers/userJoined.js
+++ src/handlers/userJoined.js
@@ -1,4 +1,4 @@
-import { postMessage } from '../libs/cometchat.js';
+import { postMessage } from '../libs/Cometchat/messageSender.js';
 import { logger } from '../utils/logging.js';
 import { roomThemes } from './message.js';
 import * as themeManager from '../utils/themeManager.js'
